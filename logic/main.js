80c0bc8f24187dad11db45ca048db4e3a6e62a9c
RichardENeumann <RichardNeumann@googlemail.com>
2023-11-06 10:32:29 +0100

    Implement Pw field congruency check pt1

--- logic/main.js
+++ logic/main.js
@@ -1,2 +1,20 @@
 "use strict";
 
+let pwInput = document.getElementById("pw");
+let pwInputConfirm = document.getElementById("pwConfirm");
+let pwError = document.getElementById("errorMessage");
+
+console.log(pwInput, pwInputConfirm,pwError);
+
+function checkCongruency() {
+    if (pwInput.value != "" && pwInput.value == pwInputConfirm.value) {
+        pwError.innerHTML = "Ok!";
+        pwError.style.color = "lightgreen";
+    } else {
+        pwError.innerHTML = "...not matching!";
+        pwError.style.color = "red";
+    }
+}
+
+pwInput.addEventListener("input", checkCongruency());
+pwInputConfirm.addEventListener("input", checkCongruency());
